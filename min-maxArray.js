3e27dc465564703f63425f0ac85c60c2319f6fd9
Nhranti Tamboli <bhrantitamboli40@gmail.com>
2026-01-07 16:21:24 +0530

    feat: add utility functions to find max and min in array

---
+++ min-maxArray.js
@@ -0,0 +1,6 @@
+const maxNumInArray=(arr)=>Math.max(...arr);
+const minNumInArray = (arr) => Math.min(...arr);
+
+const array = [1, 2, 3, 4, 5];
+console.log(maxNumInArray(array));
+console.log(minNumInArray(array));
