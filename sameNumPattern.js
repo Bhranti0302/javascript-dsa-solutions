2b9d92abc9cae352f1e183fd6ac2019fc5015ce3
Nhranti Tamboli <bhrantitamboli40@gmail.com>
2026-01-15 22:29:26 +0530

    feat: add function to print number pattern

--- sameNumPattern.js
+++ sameNumPattern.js
@@ -1,9 +1,12 @@
-let n=5;
-
-for(let i=1;i<=n;i++){
-    let row="";
-    for(let j=1;j<=i;j++){
-        row+=i
+function printNumberPattern(n) {
+  for (let i = 1; i <= n; i++) {
+    let row = "";
+    for (let j = 1; j <= i; j++) {
+      row += i;
     }
     console.log(row);
+  }
 }
+
+// Example usage
+printNumberPattern(5);
