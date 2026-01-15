0f36889ddea96a4e2a9e30d9129e94d607bcc614
Nhranti Tamboli <bhrantitamboli40@gmail.com>
2026-01-15 22:31:28 +0530

    feat: add function to print basic number triangle pattern

--- basicNumTrianglePattern.js
+++ basicNumTrianglePattern.js
@@ -1,13 +1,14 @@
-// Basic Number Triangle Pattern
+function printNumberTriangle(n) {
+  for (let i = 1; i <= n; i++) {
+    let row = "";
 
-let n = 5;
+    for (let j = 1; j <= i; j++) {
+      row += j;
+    }
 
-for (let i = 1; i <= n; i++) {
-  let row = "";
-
-  for (let j = 1; j <= i; j++) {
-    row += j;
+    console.log(row);
   }
-
-  console.log(row);
 }
+
+// Example usage
+printNumberTriangle(5);
